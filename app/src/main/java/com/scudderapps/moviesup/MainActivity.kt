7517828cfdbc218074a10394e56bae8a6cd72dda
mack997 <mayankagarwal997@gmail.com>
2020-05-06 16:55:59 +0530

    Re-arranged TabLayout

--- app/src/main/java/com/scudderapps/moviesup/MainActivity.kt
+++ app/src/main/java/com/scudderapps/moviesup/MainActivity.kt
@@ -5,26 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
+import androidx.fragment.app.FragmentTransaction
 import butterknife.BindView
 import butterknife.ButterKnife
-import com.google.android.material.tabs.TabLayout
 import com.irfaan008.irbottomnavigation.SpaceItem
 import com.irfaan008.irbottomnavigation.SpaceNavigationView
 import com.irfaan008.irbottomnavigation.SpaceOnClickListener
-import com.scudderapps.moviesup.adapter.CategoryAdapter
-import com.scudderapps.moviesup.adapter.MoviePageListAdapter
-import com.scudderapps.moviesup.api.TheTMDBApiInterface
-import com.scudderapps.moviesup.api.TheTMDBClient
 import com.scudderapps.moviesup.fragments.MovieFragment
-import com.scudderapps.moviesup.repository.movielist.MoviePagedListRepository
-import com.scudderapps.moviesup.viewmodel.MovieListViewModel
-
+import com.scudderapps.moviesup.fragments.TvFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,22 +22,9 @@ class MainActivity : AppCompatActivity() {
     @BindView(R.id.bottom_navigation_bar)
     lateinit var bottomBar: SpaceNavigationView
 
-    @BindView(R.id.viewpager)
-    lateinit var viewPager: ViewPager
-
-    @BindView(R.id.tablayout)
-    lateinit var tabLayout: TabLayout
-
-    private val fragments = ArrayList<Fragment>()
-
-    @BindView(R.id.moviesList)
-    lateinit var movieList: RecyclerView
-
-    private lateinit var listViewModel: MovieListViewModel
-    lateinit var moviePagedListRepository: MoviePagedListRepository
-    private val popularAdapter = MoviePageListAdapter(this)
-    private val linearLayoutManager = GridLayoutManager(this, 3)
-
+    lateinit var transaction: FragmentTransaction
+    lateinit var movieFragment: MovieFragment
+    lateinit var tvFragment: TvFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,59 +32,11 @@ class MainActivity : AppCompatActivity() {
         ButterKnife.bind(this)
         setSupportActionBar(toolbar)
         bottomBarSetting(savedInstanceState)
+        movieFragment = MovieFragment()
+        tvFragment = TvFragment()
 
-        val apiService: TheTMDBApiInterface = TheTMDBClient.getClient()
-        moviePagedListRepository = MoviePagedListRepository(apiService)
+        setFragment(movieFragment)
 
-        linearLayoutManager.orientation = GridLayoutManager.VERTICAL
-        linearLayoutManager.reverseLayout = false
-
-        listViewModel = movieListViewModel()
-
-
-        fragments.add(MovieFragment())
-        fragments.add(MovieFragment())
-        fragments.add(MovieFragment())
-        fragments.add(MovieFragment())
-
-        val pagerAdapter =
-            CategoryAdapter(supportFragmentManager, fragments)
-        viewPager.adapter = pagerAdapter
-
-        tabLayout.setupWithViewPager(viewPager)
-
-        tabLayout.getTabAt(0)?.text = "Trending"
-        tabLayout.getTabAt(1)?.text = "Popular"
-        tabLayout.getTabAt(2)?.text = "Now Playing"
-        tabLayout.getTabAt(3)?.text = "Upcoming"
-
-//        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab) {
-//                tabLayout.getTabAt(0)!!.icon!!.setColorFilter(
-//                    resources.getColor(android.R.color.black),
-//                    PorterDuff.Mode.SRC_IN
-//                )
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab) {
-//                tabLayout.getTabAt(2)!!.icon!!.setColorFilter(
-//                    resources.getColor(android.R.color.white),
-//                    PorterDuff.Mode.SRC_IN
-//                )
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab) {}
-//        })
-
-    }
-
-    private fun movieListViewModel(): MovieListViewModel {
-        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                @Suppress("UNCHECKED_CAST")
-                return MovieListViewModel(moviePagedListRepository) as T
-            }
-        })[MovieListViewModel::class.java]
     }
 
     private fun bottomBarSetting(savedInstanceState: Bundle?) {
@@ -132,10 +59,10 @@ class MainActivity : AppCompatActivity() {
             override fun onItemClick(itemIndex: Int, itemName: String) {
                 when (itemIndex) {
                     0 -> {
-
+                        setFragment(movieFragment)
                     }
                     1 -> {
-
+                        setFragment(tvFragment)
                     }
                     2 -> {
 
@@ -151,5 +78,12 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.tab_container, fragment)
+        transaction.commit()
+
     }
 }
--- app/src/main/java/com/scudderapps/moviesup/fragments/MovieFragment.kt
+++ app/src/main/java/com/scudderapps/moviesup/fragments/MovieFragment.kt
@@ -1,19 +1,48 @@
 package com.scudderapps.moviesup.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import butterknife.BindView
+import com.google.android.material.tabs.TabLayout
 import com.scudderapps.moviesup.R
 
 class MovieFragment() : Fragment() {
+
+    @BindView(R.id.movie_tab_layout)
+    lateinit var movieTabLayout: TabLayout
+
+    @BindView(R.id.movie_viewpager)
+    lateinit var movieViewPage: ViewPager
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.movie_fragment, container, false)
+        val view = inflater.inflate(R.layout.movie_fragment, container, false)
+
+        movieTabLayout = TabLayout(view.context)
+        movieViewPage = ViewPager(view.context)
+
+        movieTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                movieViewPage.currentItem = tab.position
+                Log.v("Clicked", tab.position.toString())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        return view
     }
 }
